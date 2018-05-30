7cc5cb0f89189b07bc0255cedef9fc0a2bd1c480
elment9 <elizabethmendoza.unmsm@gmail.com>
2018-05-30 10:45:54 -0500

    Mayus y minúsculas encode y decode ok

--- src/cipher.js
+++ src/cipher.js
@@ -1,36 +1,34 @@
 window.cipher = {
   encode:function(offset,string){
-     //Declaro palabra de usuario y extraigo la longitud de la palabra. 
     let finalText = ''; 
-    for(let i=0; i<string.length; i++){ //4.Inicio mi ciclo for
+    for(let i=0; i<string.length; i++){
       let firstAscii = string.charCodeAt(i);
-      if(firstAscii >= 65 && firstAscii <= 90){//9. Creamos condicional para Mayusculas
-         let newAsccii = (firstAscii - 65 + offset) % 26 + 65; //8. Aplicamos la fórmula y tenemos el nuevo código ASCII
-         let mayusText = String.fromCharCode(newAsccii); //String es lista global de caracteres, es parte del objeto global
-         finalText += mayusText; //11. Concatenamos : let finalText += mayusText  es igual a let finalText = finalText + mayusText
+      if(firstAscii >= 65 && firstAscii <= 90){
+         let newAsccii = (firstAscii - 65 + offset) % 26 + 65;
+         let mayusText = String.fromCharCode(newAsccii);
+         finalText += mayusText; 
       }
-      else if (firstAscii >= 97 && firstAscii <= 122){ //12. Ahora en minúscula
-         let newAsccii = (firstAscii - 97 + offset) % 26 + 
-         97;//26 porque son 26 caracteres en minuscula
+      else if (firstAscii >= 97 && firstAscii <= 122){ 
+         let newAsccii = (firstAscii - 97 + offset) % 26 + 97;
          let minusText = String.fromCharCode(newAsccii);
          finalText += minusText;
       }
-      else if (firstAscii >= 48 && firstAscii <= 57){ //14. Números
-         let newAsccii = (firstAscii - 48 + offset) % 10 + 48;//Residuo de 10 porque son 10 nùmeros
+      else if (firstAscii >= 48 && firstAscii <= 57){ 
+         let newAsccii = (firstAscii - 48 + offset) % 10 + 48;
          let numbers = String.fromCharCode(newAsccii);
          finalText += numbers;
       }
-      else if(firstAscii >= 33 && firstAscii <= 47){//15. Signos
-         let newAsccii = (firstAscii - 33 + offset) % 15 + 33;//Residuo de 15 porque son 15 signos
+      else if(firstAscii >= 33 && firstAscii <= 47){
+         let newAsccii = (firstAscii - 33 + offset) % 15 + 33;
          let sign = String.fromCharCode(newAsccii);
          finalText += sign;
       }      
-      else if(firstAscii == 32){//16. Espacios
+      else if(firstAscii == 32){
          let spaceBar = " ";
          finalText += spaceBar;
       }
       else 
-         finalText = finalText.toUpperCase(i);
+         finalText = "Ñ";
       }
     answer = finalText;
     return answer;
@@ -40,33 +38,33 @@ window.cipher = {
     for(let i=0; i<string.length; i++){
       let firstAscii = string.charCodeAt(i);
       if(firstAscii >= 65 && firstAscii <= 90){
-         let newAsccii = (firstAscii - 65 - offset) % 26 + 65;
-         let mayusText = String.fromCharCode(newAsccii); //String es lista global de caracteres, es parte del objeto global
+         let newAsccii = (firstAscii - 90 - offset) % 26 + 90;
+         let mayusText = String.fromCharCode(newAsccii); 
          finalText += mayusText; 
       }
       else if (firstAscii >= 97 && firstAscii <= 122){
-         let newAsccii = (firstAscii - 97 - offset) % 26 + 97;//26 porque son 26 caracteres en minuscula
+         let newAsccii = (firstAscii - 122 - offset) % 26 + 122;
          let minusText = String.fromCharCode(newAsccii);
          finalText += minusText;
       }
-      else if (firstAscii >= 48 && firstAscii <= 57){//14. Números
-         let newAsccii = (firstAscii - 48 - offset) % 10 + 48;//Residuo de 10 porque son 10 nùmeros
+      else if (firstAscii >= 48 && firstAscii <= 57){
+         let newAsccii = (firstAscii - 48 - offset) % 10 + 48;
          let numbers = String.fromCharCode(newAsccii);
          finalText += numbers;
       }
-      else if(firstAscii >= 33 && firstAscii <= 47){ //15. Signos
-         let newAsccii = (firstAscii - 33 - offSet) % 15 + 33;//Residuo de 15 porque son 15 signos
+      else if(firstAscii >= 33 && firstAscii <= 47){
+         let newAsccii = (firstAscii - 33 - offSet) % 15 + 33;
          let sign = String.fromCharCode(newAsccii);
          finalText += sign;
       }      
       //16. Espacios
       else if(firstAscii == 32){
-         let newAsccii = (firstAscii - 32 - offset) % 1 + 32;//Residuo de 1 porque son 1 signos
+         let newAsccii = (firstAscii - 32 - offset) % 1 + 32;
          let spaceBar = String.fromCharCode(newAsccii);
          finalText += spaceBar;
       }
       else 
-        finalText += finalText.toUpperCase(i);
+        finalText += "ñ";
     }
     answer = finalText;
     return answer;
--- src/index.js
+++ src/index.js
@@ -3,23 +3,19 @@ let string = document.getElementById("user_text");
 //Llamo botón keyNumber
 let offset = document.getElementById("offset");
 //Llamo botón codificar
-const buttonEncode = document.getElementById("encode_button");
+let buttonEncode = document.getElementById("encode_button");
 //Llamo botón decodificar
-const buttonDecode = document.getElementById("decode_button");
+let buttonDecode = document.getElementById("decode_button");
 //Llamo botón BORRADO
-const buttonErase = document.getElementById("erase_button");
+let buttonErase = document.getElementById("erase_button");
 //Llamo caja respuesta
 let answer = document.getElementById("answer_f");
 //Llamo evento Encode
 buttonEncode.addEventListener("click",() => {
-    debugger
     answer.innerHTML = cipher.encode(parseInt(offset.value),string.value);
 });
 //Llamo evento Decode
     buttonDecode.addEventListener("click",() => {
     answer.innerHTML = cipher.decode(parseInt(offset.value),string.value);
-});
-/*Llamo evento BORRADO
-buttonErase.addEventListener("click",() => {
-    string.innerHTML = ""s;
-})*/
+})
+
